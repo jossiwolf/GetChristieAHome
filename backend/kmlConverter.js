60208336763cc08c65d8c45a6e8b4e32c5195bc1
Santosh Das <santosh.das@outlook.com>
2016-10-23 03:39:34 -0500

    updating the kml converter for layers

--- backend/kmlConverter.js
+++ backend/kmlConverter.js
@@ -2,15 +2,18 @@ var options = {symbol: pointSymbol, name: 'Name'};
 
 function genPlace(shelter) {
     var placemark = '<Placemark>\n'
-    placemark += '<name>'+shelter.agency_address+'</name>\n'
-    placemark += '<styleUrl>#west_campus_style</styleUrl>'
-    placemark += ' <ExtendedData> <Data name="video"> <value><![CDATA[<iframe width="480" height="360" src="https://www.youtube.com/embed/ZE8ODPL2VPI" frameborder="0" allowfullscreen></iframe><br><br>]]></value> </Data> </ExtendedData> \n'
+    placemark += '<name>'+shelter.agency_program_name+'</name>\n'
+    placemark += '<description>'
+    placemark += 'Address: '+shelter.agency_address+'<br/>'
+    placemark += 'Capacity: '+shelter.capacity+' <br/> Occupancy: '+ shelter.occupancy +'</description>\n'
+    //placemark += '<styleUrl>#west_campus_style</styleUrl>'
     placemark += '<Point>\n'
     placemark += '<coordinates>'+shelter.longitude+','+shelter.latitude+'</coordinates>\n'
     placemark += '</Point>\n'
     placemark += '</Placemark>\n'
     return placemark
 }
+
 
 var genKML = function(shelters) {
     var mykml = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n\n';
