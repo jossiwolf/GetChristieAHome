76e3c688008caffc18075a83910a69e1f43f0ddc
jossiwolf <0003jw@heilwig.de>
2016-10-22 23:45:25 -0500

    mooore stuff

--- server.js
+++ server.js
@@ -319,5 +319,6 @@ app.get('/uber/products', function(request, response) {
     });
 });
 
-app.listen(5000);
+//app.listen(5000);
+app.listen(process.env.PORT || 5000)
 console.log("App listening on port 8080");
